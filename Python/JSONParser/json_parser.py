1e78771170658a8955336630ec5dafe9894ed98a
aleksei.kadyrov <alehak2001@gmail.com>
2025-01-07 14:59:58 +0200

    added tests

--- Python/JSONParser/json_parser.py
+++ Python/JSONParser/json_parser.py
@@ -1,6 +1,8 @@
-import json
 import re
+import json
 from enum import Enum
+from typing import Generator, Callable, Any
+from collections import namedtuple, defaultdict
 
 
 # like json.loads
@@ -10,47 +12,129 @@ class JsonGenerator:
         true = True
 
     boolean_map = {"true": BoolEnum.true, "false": BoolEnum.false}
-    level_one_comma_split = r",(?![^{}]*\}|\[[^\]]*\])"
+    null_value = "null"
 
-    def parse_object(self, object_str: str):
+    class JsonSyntaxError(Exception):
+        def __init__(self, message: str):
+            super().__init__(message)
+
+    @staticmethod
+    def _preprocess_json(json_string: str) -> str:
+        cleaned_str = ""
+        inside_string = False
+        for char in json_string:
+            if char == '"':
+                inside_string = not inside_string
+            if char == " " and not inside_string:
+                continue
+            cleaned_str += char
+
+        return cleaned_str
+
+    def _split_by_first_level_comma(self, given_str: str) -> Generator[str, None, None]:
+        current_token = ""
+        inside_object_counter = 0
+        inside_array_counter = 0
+        for c in given_str:
+            match c:
+                case ",":
+                    if not inside_object_counter and not inside_array_counter:
+                        if not current_token:
+                            raise JsonGenerator.JsonSyntaxError(
+                                "Missing value between commas."
+                            )
+                        yield current_token
+                        current_token = ""
+                    else:
+                        current_token += c
+                case "{":
+                    inside_object_counter += 1
+                    current_token += c
+                    self.bracket_stack.append("{")
+                case "}":
+                    inside_object_counter -= 1
+                    current_token += c
+                    if not self.bracket_stack or self.bracket_stack.pop() != "{":
+                        raise JsonGenerator.JsonSyntaxError("Invalid object brackets")
+                case "[":
+                    inside_array_counter += 1
+                    current_token += c
+                    self.bracket_stack.append("[")
+
+                case "]":
+                    inside_array_counter -= 1
+                    current_token += c
+                    if not self.bracket_stack or self.bracket_stack.pop() != "[":
+                        raise JsonGenerator.JsonSyntaxError("Invalid array brackets")
+                case _:
+                    current_token += c
+        # the last token
+        if current_token:
+            yield current_token
+
+    def __init__(self):
+        self.bracket_stack = []
+
+    def _parse_object(self, object_str: str):
         object_dict = {}
-        object_str = object_str.strip()[1:-1]
-        for json_pair in re.split(JsonGenerator.level_one_comma_split, object_str):
-            print(json_pair)
-            key, value = map(str.strip, json_pair.split(":", maxsplit=1))
+        if object_str[0] != "{" or object_str[-1] != "}":
+            raise JsonGenerator.JsonSyntaxError("Invalid object brackets")
+        object_str = object_str[1:-1]
+        for json_pair in self._split_by_first_level_comma(object_str):
+            if ":" not in json_pair:
+                raise JsonGenerator.JsonSyntaxError("Invalid key-value pair")
+            key, value = json_pair.split(":", maxsplit=1)
+            if key[0] != '"' or key[-1] != '"':
+                raise JsonGenerator.JsonSyntaxError("Invalid key")
             res_value = None
             # Parsing the value
-            res_value = self.parse_value(value)
+            res_value = self._parse_value(value)
             object_dict[key[1:-1]] = res_value
 
         return object_dict
 
     def parse(self, json_string: str) -> dict:
-        return self.parse_object(json_string)
+        json_string = self._preprocess_json(json_string)
+        return self._parse_object(json_string)
 
-    def parse_array(self, array_str: str) -> list:
-        pass
+    def _parse_array(self, array_str: str) -> list:
+        if array_str[0] != "[" or array_str[-1] != "]":
+            raise JsonGenerator.JsonSyntaxError("Invalid array brackets")
+        array_str = array_str[1:-1]
+        result_array = []
+        for element in self._split_by_first_level_comma(array_str):
+            result_array.append(self._parse_value(element))
+        return result_array
 
-    def parse_value(self, value: str):
-        # Handle different types of values
+    def _parse_value(self, value: str):
         if value[0] == "{":  # Nested object
-            return self.parse_object(value)
+            return self._parse_object(value)
         elif value[0] == "[":  # Nested array
-            return self.parse_array(value)
+            return self._parse_array(value)
         elif value[0] == '"':  # String
             return value[1:-1]  # Remove quotes
-        elif value in JsonGenerator.boolean_map:  # Boolean
+        elif value in JsonGenerator.boolean_map:  # Booleans
             return JsonGenerator.BoolEnum[value].value
-        elif value == "null":  # None
+        elif value == JsonGenerator.null_value:  # None
             return None
         elif "." in value:  # Float
-            return float(value)
+            try:
+                return float(value)
+            except ValueError:
+                raise JsonGenerator.JsonSyntaxError(f"Invalid float value: {value}")
         else:  # Integer
-            return int(value)
+            try:
+                return int(value)
+            except ValueError:
+                raise JsonGenerator.JsonSyntaxError(f"Invalid integer value: {value}")
 
 
 if __name__ == "__main__":
-    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null, "address": {"street": "123 Main St", "city": "Anytown", "home": 12 }}'
+    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null,"address": {"street": "123 Main St", "city": "Anytown", "home": 12}, "arr": [1,2,3,{"a":"b", "c":"d"}]}'
     # print(json.loads(test_str))
     json_parser = JsonGenerator()
-    print(json_parser.parse(test_str))
+    res_json_dict = json_parser.parse(test_str)
+    print(res_json_dict)
+
+    test_invalid_json = '{"a": "b" "c"}'
+    res_json_dict = json_parser.parse(test_invalid_json)
---
+++ Python/JSONParser/test_json_parser.py
@@ -0,0 +1,59 @@
+from json_parser import JsonGenerator
+import json
+import pytest
+
+
+# test json parser
+def test_primitive_types_json():
+    test_str = (
+        '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null}'
+    )
+    json_parser = JsonGenerator()
+    assert json_parser.parse(test_str) == json.loads(test_str)
+
+
+def test_nested_json():
+    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null,"address": {"street": "123 Main St", "city": "Anytown", "home": 12}}'
+    json_parser = JsonGenerator()
+    assert json_parser.parse(test_str) == json.loads(test_str)
+
+
+def test_array_json():
+    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null,"address": {"street": "123 Main St", "city": "Anytown", "home": 12}, "arr": [1,2,3]}'
+    json_parser = JsonGenerator()
+    assert json_parser.parse(test_str) == json.loads(test_str)
+
+
+def test_unvalid_json():
+    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null,"address": {"street": "123 Main St", "city": "Anytown", "home": 12}, "arr": [1,2,3}'
+    json_parser = JsonGenerator()
+    with pytest.raises(JsonGenerator.JsonSyntaxError):
+        json_parser.parse(test_str)
+
+
+def test_multinested_json():
+    test_str = '{"object" : {"type": {"number": 1, "string": "hello", "array": [1,2,3], "object": {"a": "b"}}}}'
+    json_parser = JsonGenerator()
+    def test_invalid_json():
+        test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": null,"address": {"street": "123 Main St", "city": "Anytown", "home": 12}, "arr": [1,2,3}'
+        json_parser = JsonGenerator()
+        with pytest.raises(JsonGenerator.JsonSyntaxError):
+            json_parser.parse(test_str)
+
+def test_missing_quotes():
+    test_str = '{"name": "Axel, "age": 30.2, "legs": 2, "is_animal": false, "trbr": null}'
+    json_parser = JsonGenerator()
+    with pytest.raises(JsonGenerator.JsonSyntaxError) as e:
+        json_parser.parse(test_str)
+
+def test_missing_comma():
+    test_str = '{"name": "Axel" "age": 30.2, "legs": 2, "is_animal": false, "trbr": null}'
+    json_parser = JsonGenerator()
+    with pytest.raises(JsonGenerator.JsonSyntaxError):
+        json_parser.parse(test_str)
+
+def test_invalid_value():
+    test_str = '{"name": "Axel", "age": 30.2, "legs": 2, "is_animal": false, "trbr": nill}'
+    json_parser = JsonGenerator()
+    with pytest.raises(JsonGenerator.JsonSyntaxError):
+        json_parser.parse(test_str)
