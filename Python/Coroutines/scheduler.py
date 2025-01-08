d1b76a6f6cb1fa1c6f2a16bdc640b065f06b42b3
aleksei.kadyrov <alehak2001@gmail.com>
2025-01-08 15:06:47 +0200

    added yield wait time

--- Python/Coroutines/scheduler.py
+++ Python/Coroutines/scheduler.py
@@ -1,4 +1,4 @@
-from typing import Callable, Generator
+from typing import Callable, Generator, Any
 from datetime import datetime, timedelta
 from priority_queue import PriorityQueue
 import time
@@ -6,11 +6,22 @@ import time
 
 class Scheduler:
 
+    class Sleep:
+        """delay for task"""
+
+        def __init__(self, seconds_to_sleep: int):
+            self.seconds_to_sleep = seconds_to_sleep
+
+        def __await__(self) -> Generator[None, None, None]:
+            start = time.time()
+            while time.time() - start < self.seconds_to_sleep:
+                yield
+            return
+
     class SchedulerTask:
-        def __init__(self, func, timepoint, frequency, *args, **kwargs):
+        def __init__(self, func, timepoint, *args, **kwargs):
             self.func = func
             self.timepoint = timepoint
-            self.frequency = frequency
             self.args = args
             self.kwargs = kwargs
 
@@ -25,31 +36,25 @@ class Scheduler:
 
     def __init__(self):
         self.task_queue = PriorityQueue()
+        self.immediate_tasks_queue = []
 
-    def add(
-        self,
-        task: Callable[..., Generator],
-        timepoint: datetime,
-        frequency: int,
-        *args,
-        **kwargs
-    ):
-        self.task_queue.push(
-            Scheduler.SchedulerTask(task, timepoint, frequency, *args, **kwargs)
-        )
+    def add(self, task: Callable[..., Any], timepoint: datetime, *args, **kwargs):
+        self.task_queue.push(Scheduler.SchedulerTask(task, timepoint, *args, **kwargs))
 
     def run(self):
-        while not self.task_queue.is_empty():
-            next_task: Scheduler.SchedulerTask = self.task_queue.pop()
+        while not (self.task_queue.is_empty() and len(self.immediate_tasks_queue) == 0):
+            # pop next task
+            if len(self.immediate_tasks_queue) != 0:
+                next_task: Scheduler.SchedulerTask = self.immediate_tasks_queue.pop(0)
+            else:
+                next_task: Scheduler.SchedulerTask = self.task_queue.pop()
             # wait until timestamp for next task
             time.sleep(max(0, (next_task.timepoint - datetime.now()).total_seconds()))
             result = next_task()
-            # if False is returned - task is not repeated
             if result != False:
-                self.add(
-                    next_task.func,
-                    next_task.timepoint + timedelta(seconds=next_task.frequency),
-                    next_task.frequency,
-                    *next_task.args,
-                    **next_task.kwargs,
-                )
+                if not (isinstance(result, int)) or result == 0:
+                    self.immediate_tasks_queue.append(next_task)
+                # if False is returned - task is not repeated
+                elif result != False:
+                    next_task.timepoint += timedelta(seconds=result)
+                    self.task_queue.push(next_task)
--- Python/Coroutines/test_scheduler.py
+++ Python/Coroutines/test_scheduler.py
@@ -9,20 +9,25 @@ def one_time_task(string: str):
 
 def returning_n_times_task(name: str):
     print(f"Hello, {name}!", datetime.now())
-    yield
+    yield 1
+
+def returning_n_times_task2(name: str):
+    print(f"Goodbuy, {name}!", datetime.now())
+    yield 2
 
 
 def test_scheduler_one_time_tasks():
     pq = Scheduler()
     test_time = datetime.now()
-    pq.add(one_time_task, test_time, 0.5, "Hello")
+    pq.add(one_time_task, test_time, "Hello")
     pq.run()
 
 
 def test_scheduler_repeating_tasks():
     pq = Scheduler()
     test_time = datetime.now()
-    pq.add(returning_n_times_task, test_time, 1, "World")
+    pq.add(returning_n_times_task, test_time, "World")
+    pq.add(returning_n_times_task2, test_time + timedelta(seconds=1), "Sir")
     pq.run()
 
 
