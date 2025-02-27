09838c3145be955617a0a6dcb8e3ca3cc4e85070
aleksei.kadyrov <alehak2001@gmail.com>
2025-02-27 13:45:34 +0200

    Fixed airflow aws etl

--- ETL/GlueTest/dags/glue_etl_aws.py
+++ ETL/GlueTest/dags/glue_etl_aws.py
@@ -17,41 +17,21 @@ with DAG(
 ) as dag:
     # Define the Glue job for process_files
     run_process_files_job = GlueJobOperator(
-        task_id='run_process_files_job',
-        job_name='process_files_etl_job',  # The name of your Glue job
-        script_location='s3://your-bucket/scripts/process_files.py',  # S3 location of your script
+        task_id='process_files_job',
+        job_name='process_files_job',  # The name of your Glue job
         aws_conn_id='aws_default',  # Your Airflow AWS connection ID
-        region_name='us-east-1',  # Your AWS region
+        region_name='eu-north-1',  # Your AWS region
         wait_for_completion=True,  # Wait for the job to complete before proceeding
-        create_job_kwargs={
-            "GlueVersion": "3.0",  # Or the version you've selected
-            "NumberOfWorkers": 2,
-            "WorkerType": "G.1X",
-            "DefaultArguments": {
-                '--job-language': 'python',
-                '--job-bookmark-option': 'job-bookmark-disable',
-            },
-        },
         dag=dag,
     )
 
     # Define the Glue job for write_to_redshift
     run_write_redshift_job = GlueJobOperator(
-        task_id='run_write_redshift_job',
-        job_name='write_to_redshift_etl_job',  # The name of your Glue job
-        script_location='s3://your-bucket/scripts/write_to_redshift.py',  # S3 location of your script
+        task_id='write_to_dynamo_script',
+        job_name='write_to_dynamo_script',  # The name of your Glue job
         aws_conn_id='aws_default',  # Your Airflow AWS connection ID
-        region_name='us-east-1',  # Your AWS region
+        region_name='eu-north-1',  # Your AWS region
         wait_for_completion=True,  # Wait for the job to complete before proceeding
-        create_job_kwargs={
-            "GlueVersion": "3.0",
-            "NumberOfWorkers": 2,
-            "WorkerType": "G.1X",
-            "DefaultArguments": {
-                '--job-language': 'python',
-                '--job-bookmark-option': 'job-bookmark-disable',
-            },
-        },
         dag=dag,
     )
 
